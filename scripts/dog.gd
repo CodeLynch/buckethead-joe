97d181d0e3da940bcafd3aa5b0f4b5cc1c2c04e9
CodeLynch <torpedonialingnena@gmail.com>
2025-09-14 22:50:45 +0800

    barking cooldown added

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -1,38 +1,51 @@
 extends CharacterBody2D
 
 @export var speed = 50
+@export var dash_bonus = 40
 @onready var sprite = $AnimatedSprite2D
+@onready var barkCD = $BarkCooldown
 
+var norm_speed = speed
+var isBarking = false
 signal bark_signal(x_pos: float)
 
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	if input_direction.x != 0:
-		if input_direction.x > 0:
-			sprite.flip_h = false
+	if not isBarking: 
+		if input_direction.x != 0:
+			if input_direction.x > 0:
+				sprite.flip_h = false
+			else:
+				sprite.flip_h = true
+			sprite.animation = "side"
+		elif input_direction.y > 0:
+			sprite.animation = "front"
+		elif input_direction.y < 0:
+			sprite.animation = "back"
 		else:
-			sprite.flip_h = true
-		sprite.animation = "side"
-	elif input_direction.y > 0:
-		sprite.animation = "front"
-	elif input_direction.y < 0:
-		sprite.animation = "back"
-	else:
-		sprite.animation = "front"
-	velocity = input_direction * speed
+			sprite.animation = "front"
+		velocity = input_direction * speed
 
 func bark():
 	bark_signal.emit(global_position.x);
-
+	sprite.animation = "bark"
+	isBarking = true
+	barkCD.start();
+	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("AButton"):
 		bark()
+	if event.is_action_pressed("BButton"):
+		speed = norm_speed + dash_bonus
+	if event.is_action_released("BButton"):
+		speed = norm_speed 
 
 func _physics_process(delta: float) -> void:
 	get_input()
-	move_and_slide()
+	if not isBarking:
+		move_and_slide()
+	
 
-
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+func _on_bark_cooldown_timeout() -> void:
+	isBarking = false 
